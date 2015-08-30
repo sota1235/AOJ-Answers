b0ccad7323dd302562a63c65463ca54a94d7472c
sota1235 <sota1235@gmail.com>
2015-08-30 19:12:41 +0900

    solve ALDS1_2 A by Ruby

---
+++ answers/Introduction/ALDS/alds1_2_a.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+# AOJ
+# ALDS1_2_A
+# Buble Sort
+
+gets
+n  = gets.split(' ').map{|i| i.to_i}
+nl = n.length
+ct = 0
+nl.times do
+  (nl-1).downto(1) do |j|
+    if n[j-1] > n[j]
+      n[j-1], n[j] = n[j], n[j-1]
+      ct += 1
+    end
+  end
+end
+
+puts n.join ' '
+puts ct
